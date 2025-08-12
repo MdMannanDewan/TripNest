782c8ae3e1f0635a9423317ff5acabd6da2ea706
MdMannanDewan <mdmannandewan.mbstu@gmail.com>
2025-08-13 03:39:30 +0600

    Basic Database Setup and data initialization completed

---
+++ init/index.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const initData = require("./data");
+const Listing = require("../models/listing");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/TripNest";
+main()
+  .then(() => console.log(`Connected Successfully`))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log(`Data was initialized`);
+};
+initDB();
